1952b24af64a637d1c540bfd8653d96a25728cd4
longblue2910 <tienlong291099@gmail.com>
2021-05-22 13:57:34 +0700

    done

---
+++ TeamNET/TeamNET/Models/Respone/Event/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamNET.Models.Respone.Event
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ TeamNET/TeamNET/Models/Respone/Student/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamNET.Models.Respone.Student
+{
+    public class Student
+    {
+        public string UserId { get; set; }
+        public string StudentCode { get; set; }
+        public string StudentName { get; set; }
+        public int CourseCurrentId { get; set; }
+        public string User { get; set; }
+    }
+}
